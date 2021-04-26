eaecca876cc17848697af7e2c117fb20238f5f78
Kibria92 <kibria92@gmail.com>
2021-04-26 22:42:31 +0600

    complete Builder Design pattern

---
+++ src/main/java/com/CrackCode/interviewQuestion/designPattern/BuilderPattern/Programmer.java
@@ -0,0 +1,16 @@
+package com.CrackCode.interviewQuestion.designPattern.BuilderPattern;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class Programmer {
+    private Long id;
+    private String name;
+    private List<String> favouriteLanguages;
+    private String skilledFrameworkName;
+    private Integer numberOfExperience;
+}
