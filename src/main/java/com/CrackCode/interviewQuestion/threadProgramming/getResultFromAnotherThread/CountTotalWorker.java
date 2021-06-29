5b19b375cefe5af39a2c49e0a23e4491dcd39751
Golam Kibria <golamkibria.java@gmail.com>
2021-06-29 17:48:17 +0600

    learn about multi thread programming assign work to one and multiple thread and get result from main thread

---
+++ src/main/java/com/CrackCode/interviewQuestion/threadProgramming/getResultFromAnotherThread/CountTotalWorker.java
@@ -0,0 +1,25 @@
+package com.CrackCode.interviewQuestion.threadProgramming.getResultFromAnotherThread;
+
+import java.util.List;
+import java.util.LongSummaryStatistics;
+
+public class CountTotalWorker extends Thread {
+    private volatile long total;//for store total {result}
+    Object object;
+
+    public void myWork(Object object) {
+        this.object = object;
+    }
+
+
+    @Override
+    public void run() {
+        List<Integer> currentWork = (List<Integer>) object;
+        LongSummaryStatistics logic = currentWork.stream().mapToLong(value -> value).summaryStatistics();
+        total = logic.getSum();
+    }
+
+    public long getTotal() {
+        return total;
+    }
+}
