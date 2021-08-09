ddf65a5875081cbd173f3d0a4e5f5c1ab7334d11
MdGolam-Kibria <golamkibria.java@gmail.com>
2021-08-10 02:37:50 +0600

    works with Collections frequency    for find how many same data in a list

---
+++ src/main/java/com/CrackCode/interviewQuestion/collection/Frequency.java
@@ -0,0 +1,16 @@
+package com.CrackCode.interviewQuestion.collection;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Frequency {
+    public static void main(String[] args) {
+        /**
+         * find how many same data in a list using Collections frequency
+         */
+        List<Integer> list = Arrays.asList(6,3,1,2,4,2,5,8,6,6,6,4,5,3);
+        int frequency = Collections.frequency(list,/*findable number*/6);
+        System.out.println(frequency);
+    }
+}
