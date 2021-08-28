ebab308a5c2e54d33f63fc18fd8c15d24c73f85b
Golam Kibria <golamkibria.java@gmail.com>
2021-08-28 19:59:21 +0600

    entityManager database to model automation Complete

---
+++ src/main/java/com/CrackCode/interviewQuestion/BeanConfig.java
@@ -0,0 +1,20 @@
+package com.CrackCode.interviewQuestion;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class BeanConfig {
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
---
+++ src/main/java/com/CrackCode/interviewQuestion/database/entityManagerHelper/InnerJoinResponseAuto.java
@@ -0,0 +1,11 @@
+package com.CrackCode.interviewQuestion.database.entityManagerHelper;
+
+import lombok.Data;
+
+@Data
+public class InnerJoinResponseAuto {
+    private Integer id;
+    private String name;
+    private String email;
+    private String childNationality;
+}
