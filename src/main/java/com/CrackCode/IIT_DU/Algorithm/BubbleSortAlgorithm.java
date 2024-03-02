c5384ca8dda27d8f5604bd65adda191e88612559
golam.kibria5290@bracits.com <golam.kibria5290@bracits.com>
2024-03-02 10:59:48 +0600

    Add BubbleSortAlgorithm

---
+++ src/main/java/com/CrackCode/IIT_DU/Algorithm/BubbleSortAlgorithm.java
@@ -0,0 +1,41 @@
+package com.CrackCode.IIT_DU.Algorithm;
+
+public class BubbleSortAlgorithm {
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    //swap elements
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 60, 35, 2, 45, 320, 5};
+
+        System.out.println("Array Before Bubble Sort");
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+
+        bubbleSort(arr);
+
+        System.out.println("Array After Bubble Sort");
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+
+        /**
+         * Time complexity :  O(N2)
+         */
+
+    }
+}
